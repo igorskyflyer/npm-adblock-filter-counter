f217aea2fcfc4e193daae2dcbe2220ea7c0a3781
igorskyflyer <igor.dvlpr@gmail.com>
2025-09-17 21:56:41 +0200

    Migrate to .ts

---
+++ test/index.test.ts
@@ -0,0 +1,30 @@
+import { assert, describe, test } from 'vitest'
+import { countFileRules, countRules } from '../src/index.js'
+
+const rules: string = `
+! {@0.1} Block these subdomains
+||ureca.samsungapps.com^
+||projectagora-d.openx.net^
+||analytics.query.yahoo.com^
+||ads-api.tiktok.com^
+||advertising.yahoo.com^
+||adtech.yahooinc.com^
+||samsungnewsapi.picks.my^
+||t.imgur.com^
+! {/@0.1}
+
+! {@1} Block these specific endpoints - global
+/cdn-cgi/trace^
+/page-view-metadata^
+/infinityid/beacon^
+`
+
+describe('🧪 adblock-filter-counter tests 🧪', () => {
+  test('#1 should return 10', () => {
+    assert.equal(countRules(rules), 11)
+  }) // #1
+
+  test('#2 should return 1609', () => {
+    assert.equal(countFileRules('./test/data/ads.txt'), 1609)
+  }) // #2
+})
